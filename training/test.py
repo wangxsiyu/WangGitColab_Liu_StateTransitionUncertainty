03418819cd8ce52bb10b0c6073fcce018482a2fb
Wang <wangs29@nih.gov>
2023-09-22 23:23:37 -0400

    u

--- training/test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import sys
-
-def func(a):
-    with open(f'savename_{a}.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-        pickle.dump([a], f)
-
-if __name__ == "__main__":
-    func(sys.argv[1])
