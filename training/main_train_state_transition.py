2bd5fea00cbb97d6d431d1c5c10b3ad12e3f7ab2
Wang <wangs29@nih.gov>
2023-09-23 16:10:19 -0400

    u

--- training/main_train_state_transition.py
+++ training/main_train_state_transition.py
@@ -3,7 +3,7 @@ from W_Trainer.W_Training_Pipeline import W_Training_Curriculum
 import sys
 
 if __name__ == "__main__":
-    sys.argv = [0,0]
+    # sys.argv = [0,0]
     assert len(sys.argv) == 2
     pip = W_Training_Curriculum()
     pip.train(seed = int(sys.argv[1]))
